f96dfea4ca231f588954d1e587650cc8c5eb46fa
Dominik Schwinn <dominik.schwinn@gmail.com>
2023-02-09 07:37:00 +0100

    docstring added to plot_airports_world_map() for documentation

--- code/airport_world_map.py
+++ code/airport_world_map.py
@@ -16,6 +16,27 @@ class Visualization():
     def plot_airports_world_map(self,
                                 airports=[],
                                 savefig=True):
+        """
+        
+
+        Parameters
+        ----------
+        airports : TYPE list, optional
+            DESCRIPTION. The default is [].
+            if airports == []: all airports from 'iata_airports_new.csv' are used (preconditioned they have lat and lon)
+            if len(airports) != 0: only the specified airports are plotted on the map
+            entries in airports must be IATA-code
+            e.g. airports = [] or airports = ['AAA','AAZ']
+        savefig : TYPE boolean, optional
+        savefig == False: map is only plotted but not saved
+        savefig == True: map is plotted and saved as *.png
+            DESCRIPTION. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
         # assert type(airports)==list
         
         self.airports = pd.read_csv('../data/iata_airports_new.csv')
@@ -26,16 +47,11 @@ class Visualization():
             self.airports = self.airports.loc[self.airports['IATA'].isin(airports), ['IATA','Airport','Location','Latitudes','Longitudes']]
 
         world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-        # world = gpd.read_file(gpd.datasets.get_path('naturalearth_highres'))
         world.explore()
-        
-        
-        
+
         gdf = gpd.GeoDataFrame(self.airports, geometry=gpd.points_from_xy(self.airports.Longitudes, self.airports.Latitudes))
-        
         # ax = world[world.continent == 'South America'].plot(color='white', edgecolor='black')
         ax = world.plot()
-        # gdf.plot(ax=ax, color='red')
         gdf.plot(ax=ax, color='red', markersize=.1)
         fig = plt.gcf()
         plt.show()
@@ -46,5 +62,5 @@ class Visualization():
 
 if __name__ == "__main__":
     x = Visualization()
-    x.plot_airports_world_map(airports=['AAA','AZZ'],
+    x.plot_airports_world_map(airports=[],
                               savefig=True)
