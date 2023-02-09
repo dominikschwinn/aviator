7fbec0739018a9c176205e4ed082e0a79fd011c8
Dominik Schwinn <dominik.schwinn@gmail.com>
2023-02-09 07:45:57 +0100

    test folder and (empty) test files added

---
+++ _tests/test_airport_data.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 07:42:36 2023
+
+@author: Dominik Schwinn
+"""
+
---
+++ _tests/test_airport_world_map.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 07:43:00 2023
+
+@author: Dominik Schwinn
+"""
+
