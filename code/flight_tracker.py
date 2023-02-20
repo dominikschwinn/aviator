7fbe6f47f99b7f54988b2f9bed9d7b7023a69d48
Dominik Schwinn <dominik.schwinn@gmail.com>
2023-02-20 21:39:25 +0100

    adding empty data frame and lists for use with flightradar API

--- code/flight_tracker.py
+++ code/flight_tracker.py
@@ -13,18 +13,71 @@ import numpy as np
 class Tracker(object):
     def __init__(self,
                  api='flightradar'):
+        """
+        
+
+        Parameters
+        ----------
+        api : TYPE (string), optional
+            DESCRIPTION. The default is 'flightradar'.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        self.ac_df = pd.DataFrame()
+        self.icao = []
+        self.operator = []
+        self.aircraft = []
+        self.origin = []
+        self.destination = []
+        self.callsign =[]
+        self.latitude = []
+        self.longitude = []
+        self.altitude = []
+        self.category = []
+        self.heading = []
 
         if api.lower() == 'flightradar':
-            self.API = FlightRadar()
+            self.API = FlightRadar(ac_df=self.ac_df,
+                                   icao=self.icao,
+                                   operator=self.operator,
+                                   aircraft=self.aircraft,
+                                   origin=self.origin,
+                                   destination=self.destination,
+                                   callsign=self.callsign,
+                                   latitude=self.latitude,
+                                   longitude=self.longitude,
+                                   altitude=self.altitude,
+                                   category=self.category,
+                                   heading=self.heading,
+                                   )
 
 ###############################################################################  
 class FlightRadar(object):
     def __init__(self,
+                 ac_df=None,
+                 icao=None,
+                 operator=None,
+                 aircraft=None,
+                 origin=None,
+                 destination=None,
+                 callsign=None,
+                 latitude=None,
+                 longitude=None,
+                 altitude=None,
+                 category=None,
+                 heading=None,
                  verbose=False):
+        """
+        (Unofficial) flight tracking API
+        - https://pypi.org/project/FlightRadarAPI/
+        - https://github.com/JeanExtreme002/FlightRadarAPI
+        """
 
         from FlightRadar24.api import FlightRadar24API
-        #https://pypi.org/project/FlightRadarAPI/
-        #https://github.com/JeanExtreme002/FlightRadarAPI
         self.fr_api = FlightRadar24API()
         #
 ###############################################################################
