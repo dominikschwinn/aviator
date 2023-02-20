a1a86dd1a973b919fb41a02a37f4bafee292b02c
Dominik Schwinn <dominik.schwinn@gmail.com>
2023-02-21 00:22:40 +0100

    added test_flight_tracker.py (initial version)

---
+++ _tests/test_flight_tracker.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 23:18:18 2023
+
+@author: Dominik Schwinn
+"""
+# import pytest
+import os,sys
+import pandas as pd
+import pickle
+import numpy as np
+
+
+class Tests:
+    print("Tests")
+    def setup_method(self):
+        pass
+
+    def test_get_flights():
+        pass
+
+    def test_flights_2_df():
+        origin = pickle.load(open('./test_files/flights_flightradar.p','rb'))
+        # result = 
