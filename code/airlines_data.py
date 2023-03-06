aefe779ca53219ad3454cb8de33c7f934db6507e
Dominik Schwinn <dominik.schwinn@gmail.com>
2023-03-06 21:23:16 +0100

    restructuring arilines_data.py

--- code/airlines_data.py
+++ code/airlines_data.py
@@ -20,6 +20,12 @@ class Airlines():
     def create_airlines_df(self,
                            verbose=True):
         self.airlines_DF = pd.DataFrame(columns=['IATA','ICAO','Airline','CallSign','Country','Comments'])
+        self.IATA = []
+        self.ICAO = []
+        self.Airline = []
+        self.CallSign = []
+        self.Country = []
+        self.Comment = []
 
     def read_url(self,
                  url='',
@@ -43,27 +49,35 @@ class Airlines():
     # for table in soup.find_all('table'):
     #     print(table.get('class'))
 
-    def fill_airlines_df(self,
-                         table=None,
-                         verbose=True):
+    def fill_airlines_df_columns(self,
+                                 table=None,
+                                 verbose=True):
         for row in table.tbody.find_all('tr'):    
             # Find all data for each column
             columns = row.find_all('td')
             
             if(columns != []):
                 iata = columns[0].text.strip()
+                print("iata:",iata)
+                print("type(iata):",type(iata))
                 icao = columns[1].text.strip()
                 airline = columns[2].text.strip()#columns[2].span.contents[0].strip('&0.')
                 callSign = columns[3].text.strip()#columns[3].span.contents[0].strip('&0.')
                 country = columns[4].text.strip()#columns[4].span.contents[0].strip('&0.')
                 comment = columns[5].text.strip()#columns[5].span.contents[0].strip('&0.')
         
-                self.airlines_DF = self.airlines_DF.append({'IATA': iata,
-                                                            'ICAO': icao,
-                                                            'Airline': airline,
-                                                            'CallSign': callSign,
-                                                            'Country': country,
-                                                            'Comment': comment}, ignore_index=True)    
+                # self.airlines_DF = self.airlines_DF.append({'IATA': iata,
+                #                                             'ICAO': icao,
+                #                                             'Airline': airline,
+                #                                             'CallSign': callSign,
+                #                                             'Country': country,
+                #                                             'Comment': comment}, ignore_index=True)
+                self.IATA.append(iata)
+                self.ICAO.append(icao)
+                self.Airline.append(airline)
+                self.CallSign.append(callSign)
+                self.Country.append(country)
+                self.Comment.append(comment)
 
 if __name__ == "__main__":
     print("Airlines")
@@ -72,5 +86,5 @@ if __name__ == "__main__":
     p = a.read_url(a.airlines_url)
     soup = a.create_bs_object(page=p)
     table = a.extract_table(soup=soup)
-    a.fill_airlines_df(table=table)
+    a.fill_airlines_df_columns(table=table)
     
